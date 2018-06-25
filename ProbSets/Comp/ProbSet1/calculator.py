ac1fb9d6f0a034c95f5c744552fefedb321ea42e
kendrarobbins <kenrob@umich.edu>
2018-06-25 17:57:05 -0500

    math PS1

---
+++ ProbSets/Comp/ProbSet1/calculator.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from math import sqrt
+def mysum(a,b):
+    return a+b
+def myproduct(a,b):
+    return a*b
+
+    
